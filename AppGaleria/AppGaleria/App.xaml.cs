fe32fbea3519cb8eda84c9b9989da94080efc44a
Elinaldo Jr <esgjunior@uesc.br>
2021-10-28 16:12:31 -0300

    Primeiro commit

---
+++ AppGaleria/AppGaleria/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGaleria
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var pg = new NavigationPage(
+                new XamarinForms.Paginas.PgNavegacao.Conteudo1()
+            );
+
+            //MainPage = new MainPage();
+            MainPage = pg;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppGaleria/AppGaleria/XamarinForms/Paginas/PgNavegacao/Conteudo2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGaleria.XamarinForms.Paginas.PgNavegacao
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Conteudo2 : ContentPage
+    {
+        public Conteudo2()
+        {
+            InitializeComponent();
+        }
+    }
+}
